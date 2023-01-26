6bc08ab1106c93d496899938158de4b371d8bcdf
Furkany04 <furkan.yildiz.k@outlook.com>
2023-01-26 22:21:12 +0300

    CarkiFelek

---
+++ carkıfelekform/carkıfelekform/Form1.Designer.cs
@@ -0,0 +1,373 @@
+﻿namespace carkıfelekform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.harf1txt = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.harftxt = new System.Windows.Forms.TextBox();
+            this.kontrolbtn = new System.Windows.Forms.Button();
+            this.harf2txt = new System.Windows.Forms.Label();
+            this.harf3txt = new System.Windows.Forms.Label();
+            this.harf4txt = new System.Windows.Forms.Label();
+            this.harf5txt = new System.Windows.Forms.Label();
+            this.harf6txt = new System.Windows.Forms.Label();
+            this.kontroletbtn = new System.Windows.Forms.Button();
+            this.bilgitxt = new System.Windows.Forms.Label();
+            this.girilenlertxt = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.haktxt = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.puantxt = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // harf1txt
+            // 
+            this.harf1txt.AutoSize = true;
+            this.harf1txt.BackColor = System.Drawing.Color.Transparent;
+            this.harf1txt.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.harf1txt.Location = new System.Drawing.Point(330, 167);
+            this.harf1txt.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.harf1txt.Name = "harf1txt";
+            this.harf1txt.Size = new System.Drawing.Size(20, 26);
+            this.harf1txt.TabIndex = 0;
+            this.harf1txt.Text = "-";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(214, 334);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Harf giriniz :";
+            // 
+            // harftxt
+            // 
+            this.harftxt.Location = new System.Drawing.Point(347, 331);
+            this.harftxt.Name = "harftxt";
+            this.harftxt.Size = new System.Drawing.Size(100, 26);
+            this.harftxt.TabIndex = 1;
+            // 
+            // kontrolbtn
+            // 
+            this.kontrolbtn.BackColor = System.Drawing.Color.Transparent;
+            this.kontrolbtn.Location = new System.Drawing.Point(546, 160);
+            this.kontrolbtn.Name = "kontrolbtn";
+            this.kontrolbtn.Size = new System.Drawing.Size(151, 34);
+            this.kontrolbtn.TabIndex = 2;
+            this.kontrolbtn.Text = "Kelimeyi oluştur";
+            this.kontrolbtn.UseVisualStyleBackColor = false;
+            this.kontrolbtn.Click += new System.EventHandler(this.kontrolbtn_Click);
+            // 
+            // harf2txt
+            // 
+            this.harf2txt.AutoSize = true;
+            this.harf2txt.BackColor = System.Drawing.Color.Transparent;
+            this.harf2txt.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.harf2txt.Location = new System.Drawing.Point(355, 167);
+            this.harf2txt.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.harf2txt.Name = "harf2txt";
+            this.harf2txt.Size = new System.Drawing.Size(20, 26);
+            this.harf2txt.TabIndex = 0;
+            this.harf2txt.Text = "-";
+            // 
+            // harf3txt
+            // 
+            this.harf3txt.AutoSize = true;
+            this.harf3txt.BackColor = System.Drawing.Color.Transparent;
+            this.harf3txt.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.harf3txt.Location = new System.Drawing.Point(380, 167);
+            this.harf3txt.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.harf3txt.Name = "harf3txt";
+            this.harf3txt.Size = new System.Drawing.Size(20, 26);
+            this.harf3txt.TabIndex = 0;
+            this.harf3txt.Text = "-";
+            // 
+            // harf4txt
+            // 
+            this.harf4txt.AutoSize = true;
+            this.harf4txt.BackColor = System.Drawing.Color.Transparent;
+            this.harf4txt.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.harf4txt.Location = new System.Drawing.Point(405, 167);
+            this.harf4txt.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.harf4txt.Name = "harf4txt";
+            this.harf4txt.Size = new System.Drawing.Size(20, 26);
+            this.harf4txt.TabIndex = 0;
+            this.harf4txt.Text = "-";
+            // 
+            // harf5txt
+            // 
+            this.harf5txt.AutoSize = true;
+            this.harf5txt.BackColor = System.Drawing.Color.Transparent;
+            this.harf5txt.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.harf5txt.Location = new System.Drawing.Point(430, 167);
+            this.harf5txt.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.harf5txt.Name = "harf5txt";
+            this.harf5txt.Size = new System.Drawing.Size(20, 26);
+            this.harf5txt.TabIndex = 0;
+            this.harf5txt.Text = "-";
+            // 
+            // harf6txt
+            // 
+            this.harf6txt.AutoSize = true;
+            this.harf6txt.BackColor = System.Drawing.Color.Transparent;
+            this.harf6txt.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.harf6txt.Location = new System.Drawing.Point(455, 167);
+            this.harf6txt.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.harf6txt.Name = "harf6txt";
+            this.harf6txt.Size = new System.Drawing.Size(20, 26);
+            this.harf6txt.TabIndex = 0;
+            this.harf6txt.Text = "-";
+            // 
+            // kontroletbtn
+            // 
+            this.kontroletbtn.BackColor = System.Drawing.Color.Transparent;
+            this.kontroletbtn.Location = new System.Drawing.Point(491, 327);
+            this.kontroletbtn.Name = "kontroletbtn";
+            this.kontroletbtn.Size = new System.Drawing.Size(141, 34);
+            this.kontroletbtn.TabIndex = 2;
+            this.kontroletbtn.Text = "Kontrol et";
+            this.kontroletbtn.UseVisualStyleBackColor = false;
+            this.kontroletbtn.Click += new System.EventHandler(this.kontroletbtn_Click);
+            // 
+            // bilgitxt
+            // 
+            this.bilgitxt.AutoSize = true;
+            this.bilgitxt.BackColor = System.Drawing.Color.Transparent;
+            this.bilgitxt.Location = new System.Drawing.Point(214, 471);
+            this.bilgitxt.Name = "bilgitxt";
+            this.bilgitxt.Size = new System.Drawing.Size(43, 20);
+            this.bilgitxt.TabIndex = 3;
+            this.bilgitxt.Text = "Bilgi";
+            // 
+            // girilenlertxt
+            // 
+            this.girilenlertxt.AutoSize = true;
+            this.girilenlertxt.Location = new System.Drawing.Point(317, 535);
+            this.girilenlertxt.Name = "girilenlertxt";
+            this.girilenlertxt.Size = new System.Drawing.Size(0, 20);
+            this.girilenlertxt.TabIndex = 3;
+            this.girilenlertxt.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(214, 535);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Girilen harfler :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(455, 167);
+            this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 26);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "-";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(430, 167);
+            this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 26);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "-";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(405, 167);
+            this.label5.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(20, 26);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "-";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(380, 167);
+            this.label6.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(20, 26);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "-";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(355, 167);
+            this.label7.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(20, 26);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "-";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(330, 167);
+            this.label8.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(20, 26);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "-";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Location = new System.Drawing.Point(43, 49);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(50, 20);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "HAK:";
+            // 
+            // haktxt
+            // 
+            this.haktxt.AutoSize = true;
+            this.haktxt.BackColor = System.Drawing.Color.Transparent;
+            this.haktxt.Location = new System.Drawing.Point(94, 49);
+            this.haktxt.Name = "haktxt";
+            this.haktxt.Size = new System.Drawing.Size(29, 20);
+            this.haktxt.TabIndex = 10;
+            this.haktxt.Text = "10";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.Transparent;
+            this.label10.Location = new System.Drawing.Point(43, 86);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(55, 20);
+            this.label10.TabIndex = 10;
+            this.label10.Text = "Puan:";
+            // 
+            // puantxt
+            // 
+            this.puantxt.AutoSize = true;
+            this.puantxt.BackColor = System.Drawing.Color.Transparent;
+            this.puantxt.Location = new System.Drawing.Point(94, 86);
+            this.puantxt.Name = "puantxt";
+            this.puantxt.Size = new System.Drawing.Size(19, 20);
+            this.puantxt.TabIndex = 10;
+            this.puantxt.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(950, 589);
+            this.Controls.Add(this.puantxt);
+            this.Controls.Add(this.haktxt);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.girilenlertxt);
+            this.Controls.Add(this.bilgitxt);
+            this.Controls.Add(this.kontroletbtn);
+            this.Controls.Add(this.kontrolbtn);
+            this.Controls.Add(this.harftxt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.harf6txt);
+            this.Controls.Add(this.harf5txt);
+            this.Controls.Add(this.harf4txt);
+            this.Controls.Add(this.harf3txt);
+            this.Controls.Add(this.harf2txt);
+            this.Controls.Add(this.harf1txt);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label harf1txt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox harftxt;
+        private System.Windows.Forms.Button kontrolbtn;
+        private System.Windows.Forms.Label harf2txt;
+        private System.Windows.Forms.Label harf3txt;
+        private System.Windows.Forms.Label harf4txt;
+        private System.Windows.Forms.Label harf5txt;
+        private System.Windows.Forms.Label harf6txt;
+        private System.Windows.Forms.Button kontroletbtn;
+        private System.Windows.Forms.Label bilgitxt;
+        private System.Windows.Forms.Label girilenlertxt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label haktxt;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label puantxt;
+    }
+}
+
---
+++ carkıfelekform/carkıfelekform/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace carkıfelekform
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        char[] harfler = new char[31] ;
+        Random rastgele = new Random();
+        
+        string[] girilenler = new string[31];
+        int sayac = 0;
+        int hakgg = 10;
+        int puan = 0;
+        private void kontrolbtn_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                harfler[i] =Convert.ToChar(rastgele.Next(97, 123));
+
+            }
+
+            harf1txt.Text = harfler[0].ToString();
+            harf2txt.Text = harfler[1].ToString();
+            harf3txt.Text = harfler[2].ToString();
+            harf4txt.Text = harfler[3].ToString();
+            harf5txt.Text = harfler[4].ToString();
+            harf6txt.Text = harfler[5].ToString();
+
+
+
+
+            harf1txt.Visible = false;
+            harf2txt.Visible = false;
+            harf3txt.Visible = false;
+            harf4txt.Visible = false;
+            harf5txt.Visible = false;
+            harf6txt.Visible = false;
+            kontrolbtn.Text = "Kelime oluştu";
+            
+
+        }
+
+        private void kontroletbtn_Click(object sender, EventArgs e)
+        {
+            
+            if(harftxt.Text == harf1txt.Text)
+            {
+                harf1txt.Visible=true;
+                bilgitxt.Text = harftxt.Text + " harfi kelimenin 1. harfidir";
+                label8.Visible= false;
+                harf1txt.Visible = true;
+                puan += 10;
+                puantxt.Text = puan.ToString();
+            }
+            else if (harftxt.Text == harf2txt.Text)
+            {
+                harf2txt.Visible=true;
+                bilgitxt.Text = harftxt.Text + " harfi kelimenin 2. harfidir";
+                label7.Visible = false;
+                harf2txt.Visible = true;
+                puan += 10;
+                puantxt.Text = puan.ToString();
+            }
+            else if (harftxt.Text == harf3txt.Text)
+            {
+                harf3txt.Visible = true;
+                bilgitxt.Text = harftxt.Text + " harfi kelimenin 3. harfidir";
+                label6.Visible = false;
+                harf3txt.Visible = true;
+                puan += 10;
+                puantxt.Text = puan.ToString();
+            }
+
+
+
+            else if (harftxt.Text == harf4txt.Text)
+            {
+                harf4txt.Visible = true;
+                bilgitxt.Text = harftxt.Text + " harfi kelimenin 4. harfidir";
+                label5.Visible = false;
+                harf4txt.Visible = true;
+                puan += 10;
+                puantxt.Text = puan.ToString();
+            }
+
+            else if (harftxt.Text == harf5txt.Text)
+            {
+                harf5txt.Visible = true;
+                bilgitxt.Text = harftxt.Text + " harfi kelimenin 5. harfidir";
+                label4.Visible = false;
+                harf5txt.Visible = true;
+                puan += 10;
+                puantxt.Text = puan.ToString();
+            }
+
+            else if (harftxt.Text == harf6txt.Text)
+            {
+                harf6txt.Visible = true;
+                bilgitxt.Text = harftxt.Text + " harfi kelimenin 6. harfidir";
+                label3.Visible = false;
+                harf6txt.Visible = true;
+                puan += 10;
+                puantxt.Text = puan.ToString();
+            }
+            else
+            {
+                bilgitxt.Text = harftxt.Text +" harfi kelimede yoktur!";
+                girilenler[sayac] = harftxt.Text;
+                girilenlertxt.Text += girilenler[sayac] + ",";
+                hakgg--;
+            }
+            
+            if(hakgg< 0)
+            {
+                MessageBox.Show("Oyun bitti");
+                Environment.Exit(0);
+            }
+            if(puan == 60)
+            {
+                MessageBox.Show("Tebrikler oyunu kazandınız");
+                Environment.Exit(0);
+            }
+            
+            haktxt.Text = hakgg.ToString();
+            harftxt.Clear();
+            
+            girilenlertxt.Visible = true;
+            sayac++;
+            
+
+        }
+    }
+}
